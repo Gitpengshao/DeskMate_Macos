1fe320a2c1a789fd5201eeedc44987b44671fa01
tanzhipeng <tanzhipeng@acrossbyte.com>
2026-06-23 22:48:40 +0800

    Initial Commit

---
+++ DeskMate/DeskMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskMateApp.swift
+//  DeskMate
+//
+//  Created by mac002 on 2026/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DeskMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
